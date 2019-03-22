c183201503548d0d35fabb9e2648d9fe326b4ec6
Erlend Eriksen <erlend_ne@hotmail.com>
2019-03-22 03:42:43 +0000

    working image acquisition

---
+++ utils.h
@@ -0,0 +1,21 @@
+#include <sys/time.h>
+#include <stdio.h>
+
+namespace utils
+{
+  inline long ms(){
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+          
+    long ms = tp.tv_sec*1000 + tp.tv_usec/1000;
+    return ms;
+  }     
+
+  inline long tic(long& start){
+    long now = ms();
+    long dt = now - start;
+    start = now;
+    printf("toc: %ld\n", dt);
+    return dt;
+  }
+}
